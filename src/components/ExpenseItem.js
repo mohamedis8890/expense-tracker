6fa28961d1a52b359e8b486296b84eaa152dc91e
mohamedis8890 <mohamed.is8890@gmail.com>
2022-01-31 22:33:53 +0200

    Split ExpenseItem into ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
- 
   return (
     <div className="expense-item">
-      <div>{props.date.toLocaleString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
